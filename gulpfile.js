f735590440636728970875b659a057df263c08f5
Timothy Jensen <tim@timjensen.us>
2017-03-10 13:30:14 -0500

    Added browser sync.

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,21 @@
 
 var gulp = require('gulp'),
 
-	// Sass/CSS processes
-	bourbon = require('bourbon').includePaths,
-	neat = require('bourbon-neat').includePaths,
-	sass = require('gulp-sass'),
-	postcss = require('gulp-postcss'),
-	autoprefixer = require('gulp-autoprefixer'),
-	sourcemaps = require('gulp-sourcemaps'),
-	cssMinify = require('gulp-cssnano'),
-	sassLint = require('gulp-sass-lint'),
+    // Sass/CSS processes
+    autoprefixer = require('autoprefixer'),
+    bourbon = require('bourbon').includePaths,
+    cssMinify = require('gulp-cssnano'),
+    neat = require('bourbon-neat').includePaths,
+    postcss = require('gulp-postcss'),
+    sass = require('gulp-sass'),
+    sassLint = require('gulp-sass-lint'),
+    sourcemaps = require('gulp-sourcemaps'),
 
-	// Utilities
-	rename = require('gulp-rename'),
-	notify = require('gulp-notify'),
-	plumber = require('gulp-plumber');
+    // Utilities
+    browserSync = require('browser-sync'),
+    notify = require('gulp-notify'),
+    rename = require('gulp-rename'),
+    plumber = require('gulp-plumber');
 
 /*************
  * Utilities
@@ -27,18 +28,18 @@ var gulp = require('gulp'),
  * @function
  */
 function handleErrors() {
-	var args = Array.prototype.slice.call(arguments);
+    var args = Array.prototype.slice.call(arguments);
 
-	notify.onError({
-		title: 'Task Failed [<%= error.message %>',
-		message: 'See console.',
-		sound: 'Sosumi' // See: https://github.com/mikaelbr/node-notifier#all-notification-options-with-their-defaults
-	}).apply(this, args);
+    notify.onError({
+        title: 'Task Failed [<%= error.message %>',
+        message: 'See console.',
+        sound: 'Sosumi' // See: https://github.com/mikaelbr/node-notifier#all-notification-options-with-their-defaults
+    }).apply(this, args);
 
-	gutil.beep(); // Beep 'sosumi' again
+    gutil.beep(); // Beep 'sosumi' again
 
-	// Prevent the 'watch' task from stopping
-	this.emit('end');
+    // Prevent the 'watch' task from stopping
+    this.emit('end');
 }
 
 
@@ -51,70 +52,76 @@ function handleErrors() {
  */
 gulp.task('postcss', function(){
 
-	return gulp.src('assets/sass/style.scss')
+    return gulp.src('assets/sass/style.scss')
 
-           // Error handling
-           .pipe(plumber({
-                errorHandler: handleErrors
-           }))
+    // Error handling
+        .pipe(plumber({
+            errorHandler: handleErrors
+        }))
 
-	           // Wrap tasks in a sourcemap
-			.pipe( sourcemaps.init())
+        // Wrap tasks in a sourcemap
+        .pipe( sourcemaps.init())
 
-			.pipe( sass({
-				includePaths: [].concat( bourbon, neat ),
-				errLogToConsole: true,
-				outputStyle: 'expanded' // Options: nested, expanded, compact, compressed
-			}))
+        .pipe( sass({
+            includePaths: [].concat( bourbon, neat ),
+            errLogToConsole: true,
+            outputStyle: 'expanded' // Options: nested, expanded, compact, compressed
+        }))
 
-		   .pipe( postcss([
-			   autoprefixer({
-				   browsers: ['last 2 versions']
-			   })
-		   ]))
+        .pipe( postcss([
+            autoprefixer({
+                browsers: ['last 2 versions']
+            })
+        ]))
 
-			// creates the sourcemap
-			.pipe(sourcemaps.write())
+        // creates the sourcemap
+        .pipe(sourcemaps.write())
 
-			.pipe(gulp.dest('./'));
+        .pipe(gulp.dest('./'))
+        .pipe(browserSync.stream());
 
 });
 
 gulp.task('css:minify', ['postcss'], function() {
-	return gulp.src('style.css')
-       // Error handling
-       .pipe(plumber({
-           errorHandler: handleErrors
-       }))
+    return gulp.src('style.css')
+    // Error handling
+        .pipe(plumber({
+            errorHandler: handleErrors
+        }))
 
-		.pipe( cssMinify({
-			safe: true
-		}))
-       .pipe(rename('style.min.css'))
-       .pipe(gulp.dest('./'))
-		.pipe(notify({
-			message: 'Styles are built.'
-		}))
+        .pipe( cssMinify({
+            safe: true
+        }))
+        .pipe(rename('style.min.css'))
+        .pipe(gulp.dest('./'))
+        // .pipe(notify({
+        //     message: 'Styles are built.'
+        // }))
 });
 
 gulp.task('sass:lint', ['css:minify'], function() {
-	gulp.src([
-		'assets/sass/style.scss',
-		'!assets/sass/base/html5-reset/_normalize.scss',
-		'!assets/sass/utilities/animate/**/*.*'
-	])
-		.pipe(sassLint())
-		.pipe(sassLint.format())
-		.pipe(sassLint.failOnError())
+    gulp.src([
+        'assets/sass/style.scss',
+        '!assets/sass/base/html5-reset/_normalize.scss',
+        '!assets/sass/utilities/animate/**/*.*'
+    ])
+        .pipe(sassLint())
+        .pipe(sassLint.format())
+        .pipe(sassLint.failOnError())
 });
-
 
 /********************
  * All Tasks Listeners
  *******************/
 
 gulp.task('watch', function () {
-	gulp.watch('assets/sass/**/*.scss', ['styles']);
+    // Kick off BrowserSync.
+    browserSync.init({
+        proxy: "www.wpsite.dev"
+    });
+
+    gulp.watch('assets/sass/**/*.scss', ['styles']);
+    gulp.watch('./**/*.php',browserSync.reload);
 });
 
 /**
@@ -122,3 +129,4 @@ gulp.task('watch', function () {
  */
 // gulp.task('scripts', [''])
 gulp.task('styles', ['sass:lint'] );
+gulp.task('default',['sass:lint', 'watch']);
