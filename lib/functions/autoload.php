e89f5dd28988d6950bb63a4803ea062555917549
Timothy Jensen <tim@timjensen.us>
2017-03-11 20:23:36 -0500

    Updated responsive menu script and styles.

--- lib/functions/autoload.php
+++ lib/functions/autoload.php
@@ -31,7 +31,7 @@ function load_nonadmin_files() {
 		'components/customizer/css-handler.php',
 		'components/customizer/helpers.php',
 		'functions/formatting.php',
-		'functions/load-assets.php',
+		'functions/enqueue.php',
 		'functions/markup.php',
 //		'structure/archive.php',
 		'structure/comments.php',
---
+++ lib/functions/enqueue.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Asset loader handler.
+ *
+ * @package     TimJensen\GenesisStarter
+ * @since       1.0.0
+ * @author      Tim Jensen
+ * @link        https://www.timjensen.us
+ * @license     GNU General Public License 2.0+
+ */
+namespace TimJensen\GenesisStarter;
+
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_assets' );
+/**
+ * Enqueue Scripts and Styles.
+ *
+ * @since 1.0.2
+ *
+ * @return void
+ */
+function enqueue_assets() {
+
+	wp_enqueue_style( CHILD_TEXT_DOMAIN . '-fonts', '//fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700', array(), CHILD_THEME_VERSION );
+
+	wp_enqueue_style( 'dashicons' );
+
+	$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
+	wp_enqueue_script( 'genesis-sample-responsive-menu', get_stylesheet_directory_uri() . "/assets/js/responsive-menus{$suffix}.js", array( 'jquery' ), CHILD_THEME_VERSION, true );
+	wp_localize_script(
+		'genesis-sample-responsive-menu',
+		'genesis_responsive_menu',
+		responsive_menu_settings()
+	);
+
+}
+
+/**
+ * Defines the responsive menu settings.
+ *
+ * @return array
+ */
+function responsive_menu_settings() {
+
+	return array(
+		'mainMenu'          => __( 'Menu', 'genesis-sample' ),
+		'menuIconClass'     => 'dashicons-before dashicons-menu',
+		'subMenu'           => __( 'Submenu', 'genesis-sample' ),
+		'subMenuIconsClass' => 'dashicons-before dashicons-arrow-down-alt2',
+		'menuClasses'       => array(
+			'combine' => array(
+				'.nav-primary',
+				'.nav-header',
+			),
+			'others'  => array(),
+		),
+	);
+
+}
